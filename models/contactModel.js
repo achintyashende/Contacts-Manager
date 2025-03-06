84af7c815d119dad8f9224dee08d32a3921f30f6
Achintya <achintyashende@gmail.com>
2025-03-06 10:39:52 +0530

    Created Contact Schema

---
+++ models/contactModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const contactSchema = mongoose.Schema({
+    name: {
+        type: stringify,
+        required: [true, "Please enter a name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter a valid email"],
+        unique: true
+    },
+    phone: {
+        type: String,
+        required: [true, "Please enter a phone number"]
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Contact', contactSchema)
